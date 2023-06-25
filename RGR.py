65fc668b521917b0d119aefea5cda0ff287ccb49
nimruzin <49985455+nimruzin@users.noreply.github.com>
2023-06-25 17:13:59 +0700

    new_rgr.py

--- RGR.py
+++ RGR.py
@@ -1,15 +1,19 @@
 from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox as mb
+from shamir import encrypt, decrypt
+from RSA import encode_rsa, decode_rsa
+
 
 
 class Rgr():
-    
+
     def __init__(self):
         self.root = Tk()
         self.f = Frame()
         self.b1 = Button(self.f, width = 21, height = 2, text = "Ввод и Сохранение файла", command = self.input_text)
-        self.b2 = Button(self.f, width = 21, height = 2, text = "Кодирование текста", command = self.encode_text)
-        self.b3 = Button(self.f, width = 21, height = 2, text = "Декодирование текста", command = self.decode_text)
-        self.b4 = Button(self.f, width = 21, height = 2, text = "Вывод файла", command = self.print_file)
+        self.b2 = Button(self.f, width = 21, height = 2, text = "Кодирование текста", command = self.shamir_encode)
+        self.b3 = Button(self.f, width = 21, height = 2, text = "Декодирование текста", command = self.shamir_decode)
 
         self.canvas = Canvas(self.f, width = 120, height = 50)
 
@@ -20,7 +24,6 @@ class Rgr():
         self.b1.pack()
         self.b2.pack()
         self.b3.pack()
-        self.b4.pack()
 
         self.animate()
 
@@ -30,9 +33,6 @@ class Rgr():
             self.canvas.coords(self.text1, 200, 20)
         self.root.after(100, self.animate)
 
- 
-
-    # Функция ввода текста
     def input_text(self):
         self.f2 = Frame()
         self.l1 = Label(self.f2, width = 15, height = 2, text = "Ввведите текст")
@@ -50,21 +50,18 @@ class Rgr():
 
         self.filename = self.e2.get()
 
-        
     def input_text_b(self):
         with open(self.e2.get(), "w", encoding="utf-8") as self.file:
             self.file.write(self.e1.get())
         self.f2.destroy()
 
-    # Функция кодирования
-    def encode_text(self):
+    def shamir_encode(self):  
         self.f3 = Frame()
         self.l3 = Label(self.f3, width = 21, height = 2, text = "Ввведите секретное слово")
         self.e3 = Entry(self.f3)
         self.l4 = Label(self.f3, width = 15, height = 2, text = "Введите имя файла")
         self.e4 = Entry(self.f3)
-        self.encoded_text = ""
-        self.b6 = Button(self.f3, width = 15, height = 2, text = "Закодировать", command = self.encode_text_b)
+        self.b6 = Button(self.f3, width = 15, height = 2, text = "Закодировать", command = self.shamir_encode_text_b)
 
         self.f3.pack()
         self.l3.pack()
@@ -74,54 +71,60 @@ class Rgr():
         self.b6.pack()
 
 
-    def encode_text_b(self):
+    def shamir_encode_text_b(self):
         with open(self.e4.get(), "r", encoding="utf-8") as self.file:
             self.text = self.file.read()
-            
-        self.password = self.e3.get()
-        
-        for i in range(len(self.text)):
-            self.nowChar = self.text[i]
-            self.secretChar = self.password[i % len(self.password)]
-            self.encoded_char = chr(ord(self.nowChar) + ord(self.secretChar))
-            self.encoded_text += self.encoded_char
-            
+
+        self.x = encrypt(self.text)
+
         self.encoded_filename = "encoded_" + self.e4.get()
+
         with open(self.encoded_filename, "w", encoding="utf-8") as self.file:
-            self.file.write(self.password + "\n")
-            self.file.write(self.encoded_text)
+            for i in self.x:
+                self.file.write('%s\n' % i)
+
+        
         self.f3.destroy()
 
+    def shamir_decode(self):
+        self.f4 = Frame()
+        self.l5 = Label(self.f4, width = 21, height = 2, text = "Ввведите секретное слово")
+        self.e5 = Entry(self.f4)
+        self.password = self.e5.get()
+        self.l6 = Label(self.f4, width = 15, height = 2, text = "Введите имя файла")
+        self.e6 = Entry(self.f4)
+        self.encoded_text = ""
+        self.b7 = Button(self.f4, width = 15, height = 2, text = "Декодировать", command = self.shamir_decode_text_b)
+
+        self.f4.pack()
+        self.l5.pack()
+        self.e5.pack()
+        self.l6.pack()
+        self.e6.pack()
+        self.b7.pack()
 
 
+    def shamir_decode_text_b(self):
+        self.decode = "encoded_" + self.e6.get()
+        self.decode1 = self.e6.get()
+        self.text_decode = list()
+        with open(self.decode, "r", encoding = "utf-8") as self.file:
+            for line in self.file:
+                currentPlace = line[:-1]
+                self.text_decode.append(int(currentPlace))
 
-    # Функция декодирования
-    def decode_text(self):
-        decoded_text = ""
-        with open(filename, "r", encoding="utf-8") as file:
-            file_password = file.readline().strip()
-            if password != file_password:
-                print("Неверный пароль. Декодирование невозможно.")
-                return
-            encoded_text = file.read()
-        for i in range(len(encoded_text)):
-            if encoded_text[i].isalnum():
-                nowChar = encoded_text[i]
-                secretChar = password[i % len(password)]
-                decoded_char = chr(ord(nowChar) - ord(secretChar))
-                decoded_text += decoded_char
-            else:
-                decoded_text += encoded_text[i]
-        decoded_filename = "decoded_" + filename[8:]
-        with open(decoded_filename, "w", encoding="utf-8") as file:
-            file.write(decoded_text)
-        print("Текст успешно декодирован и сохранен в файл", decoded_filename)
+        print(self.text_decode)
 
-    # Функция печати файла на экране
-    def print_file(self):
-        with open(filename, "r", encoding="utf-8") as file:
-            text = file.read()
-        print(text)
+        self.z = decrypt(self.text_decode)
+
+            
+        self.decoded_filename = "decoded_" + self.decode1
+        with open(self.decoded_filename, "w", encoding="utf-8") as self.file:
+            self.file.write(self.z)
+        mb.showinfo(":)", "Text successfully decoded")
+
+
 
 x = Rgr()
+
 x.root.mainloop()
